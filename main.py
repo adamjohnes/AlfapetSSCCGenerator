31b22d69d6ef662b92b04c55d00bf3b6cfad0828
adamjohnes <70388982+adamjohnes@users.noreply.github.com>
2022-12-17 20:55:42 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -13,6 +13,49 @@ from PIL import Image, ImageDraw, ImageFont
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
 
+addrHelps = "7035: Alachua, FL\n\
+7033: Apple Valley, CA\n\
+7039: Beaver Dam, WI\n\
+6094: Bentonville, AR\n\
+6011: Brookhaven, MS\n\
+6020: Brooksville, FL\n\
+6031: Buckeye, AZ\n\
+6043: Coldwater, MI\n\
+6006: Cullman, AL\n\
+6010: Douglas, GA\n\
+7038: Ft Pierce, FL\n\
+7026: Grantsville, UT\n\
+6024: Grove City, OH\n\
+6037: Hermiston, OR\n\
+6040: Hope Mills, NC\n\
+6066: Hopkinsville, KY\n\
+6054: La Grange, GA\n\
+6019: Loveland, CO\n\
+6038: Marcy, NY\n\
+6025: Menomonie, WI\n\
+6039: Midway, TN\n\
+7045: Mt Crawford, VA\n\
+6009: Mount Pleasant, IA\n\
+6016: New Braunfels, TX\n\
+6048: Opelousas, LA\n\
+6035: Ottawa, KS\n\
+6036: Palestine, TX\n\
+6012: Plainview, TX\n\
+6021: Porterville, CA\n\
+6030: Raymond, NH\n\
+6026: Red Bluff, CA\n\
+6068: Sanger, TX\n\
+7036: Sealy, TX\n\
+6018: Searcy, AZ\n\
+6017: Seymour, IN\n\
+6070: Shelby, NC\n\
+7034: Smyrna, DE\n\
+6092: Spring Valley, IL\n\
+6069: St. James, MO\n\
+6023: Sutherland, VA\n\
+6080: Tobyhanna, PA\n\
+6027: Woodland, PA"
+
 destAddresses = {
 		7035: "Alachua, FL",
 		7033: "Apple Valley, CA",
@@ -62,8 +105,8 @@ destAddress = str(); poNum = str();
 step1 = False; step2 = False; single = True
 
 root = customtkinter.CTk()
-root.geometry("700x550")
-root.title("Random Number Generator")
+root.geometry("600x750")
+root.title("Label Generator")
 
 def seeFiles():
 	filedialog.askopenfilename(initialdir = os.getcwd())
@@ -71,36 +114,51 @@ def seeFiles():
 def getDestAddress():
 	global step1, destAddress
 
-	dialog = customtkinter.CTkInputDialog(text = "Enter a 4-digit destination Address (i.e. 7033 for Apple Valley)", title = "Enter a 4-digit destination address")
-	while not destAddress.isdigit() or len(destAddress) != 4 or destAddresses.get(int(destAddress), "Not Found") == "Not Found":
-		try:	
-			destAddress = dialog.get_input()
-		except Exception as e:
-			textbox.delete(0.0, 100.0)
-			textbox.insert(index = 0.0, text = "Please write a known four-digit\nnumber!", tags = None)
-			return
-	textbox.delete(0.0, 100.0)
-	destAddress = destAddresses.get(int(destAddress))
-	step1 = True
+	try:
+		destText.delete(0.0, 100.0)
+		dialog = customtkinter.CTkInputDialog(text = "Enter a 4-digit destination Address (i.e. 7033 for Apple Valley)", title = "Enter a 4-digit destination address")
+
+		while not destAddress.isdigit() or len(destAddress) != 4 or destAddresses.get(int(destAddress), "Not Found") == "Not Found":
+			try:	
+				destAddress = dialog.get_input()
+			except Exception as e:
+				textbox.delete(0.0, 100.0)
+				textbox.insert(index = 0.0, text = "Please write a known four-digit\nnumber!", tags = None)
+				return
+		textbox.delete(0.0, 100.0)
+		destAddress = destAddresses.get(int(destAddress))
+		destText.insert(index = 0.0, text = destAddress)
+		step1 = True
+	except Exception as e:
+		quit()
 
 def getPONum():
 	global step2, poNum
 
-	dialog = customtkinter.CTkInputDialog(text = "Enter a valid PO number", title = "Enter a PO")
-	while not poNum.isdigit():
-		try:	
-			poNum = dialog.get_input()
-		except Exception as e:
-			textbox.delete(0.0, 100.0)
-			textbox.insert(index = 0.0, text = "Please write a valid PO\nnumber!", tags = None)
-			return
-	textbox.delete(0.0, 100.0)
-	step2 = True
+	try:
+		poText.delete(0.0, 100.0)
+		dialog = customtkinter.CTkInputDialog(text = "Enter a valid PO number", title = "Enter a PO")
+		while not poNum.isdigit():
+			try:	
+				poNum = dialog.get_input()
+			except Exception as e:
+				textbox.delete(0.0, 100.0)
+				textbox.insert(index = 0.0, text = "Please write a valid PO\nnumber!", tags = None)
+				return
+		textbox.delete(0.0, 100.0)
+		poText.insert(index = 0.0, text = poNum)
+		step2 = True
+	except Exception as e:
+		quit()
+
+def seeAddrs():
+	dialog = customtkinter.CTkTextbox(master = frame, width = 200, height = 120)
+	dialog.insert(index = 0.0, text = addrHelps)
+	dialog.pack(pady = 5)
 
 def mixedFunc():
 	global single
 	single = not single
-	print(single)
 
 def createBarcode(number : str):
 	global step1, step2, destAddress, poNum, single
@@ -159,6 +217,8 @@ def createBarcode(number : str):
 	new_image.save(".\\barcodes\\" + number + "_barcode.png", "PNG")
 	step1 = False
 	step2 = False
+	poText.delete(0.0, 100.0)
+	destText.delete(0.0, 100.0)
 
 def generateNumber():
 
@@ -202,12 +262,21 @@ label.pack(pady = 2, padx = 10)
 button = customtkinter.CTkButton(master = frame, text = "Enter 4-digit Address", command = getDestAddress)
 button.pack(pady = 10, padx = 0.5)
 
+destText = customtkinter.CTkTextbox(master = frame, width = 140, height = 25)
+destText.pack()
+
 button = customtkinter.CTkButton(master = frame, text = "Enter PO Number", command = getPONum)
-button.pack(pady = 20, padx = 0.5)
+button.pack(pady = 10, padx = 0.5)
+
+poText = customtkinter.CTkTextbox(master = frame, width = 140, height = 25)
+poText.pack()
 
 checkbox = customtkinter.CTkCheckBox(master = frame, text = "Mixed? (only check if mixed)", command = mixedFunc,
                                      onvalue="on", offvalue="off")
 checkbox.pack(padx=20, pady=10)
+
+entry = customtkinter.CTkLabel(master = frame, text = "SSCC-18 Number:")
+entry.pack()
 
 textbox = customtkinter.CTkTextbox(master = frame, width = 200, height = 50)
 textbox.pack(pady = 5, padx = 5)
@@ -215,13 +284,13 @@ textbox.pack(pady = 5, padx = 5)
 button = customtkinter.CTkButton(master = frame, text = "Generate Label!", command = generateNumber)
 button.pack(pady = 25, padx = 10)
 
-label = customtkinter.CTkLabel(master = frame, text = "See file containing all generated numbers below")
+label = customtkinter.CTkLabel(master = frame, text = "See folder containing all generated barcodes below")
 label.pack(pady = 2, padx = 2)
 
-button = customtkinter.CTkButton(master = frame, text = "See numberlog.txt", command = seeFiles)
+button = customtkinter.CTkButton(master = frame, text = "See barcodes", command = seeFiles)
 button.pack(pady = 2, padx = 2)
 
-label = customtkinter.CTkLabel(master = frame, text = "(you may have to right-click on numberlog.txt and select 'open')")
-label.pack(pady = 20, padx = 2)
+helper = customtkinter.CTkButton(master = frame, text = "See 4-digit addresses here", command = seeAddrs)
+helper.pack(pady = 20, padx = 0)
 
 root.mainloop()
